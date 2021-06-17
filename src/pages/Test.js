58bdaf658d6a7e98bed63161906aa59d48203ee0
Petro <pro100vaness@gmail.com>
2021-06-18 03:25:51 +1000

    slides final

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -83,8 +83,15 @@ const Test = ({}) => {
         return res.json();
       })
       .then((re) => {
-        if (ok) setSlides(re.filter((item) => !item.is_archive));
-        else if (status == 401) history.push("/");
+        if (ok) {
+          let res = []
+          re.map(item => {
+            if(!item.is_archive && parseFloat(item.remains) > parseFloat(item.coast)) {
+              res = [...res, item]
+            }
+          })
+          setSlides(res);
+        } else if (status == 401) history.push("/");
         else alert(JSON.stringify(re) + " " + status);
       });
   }, []);
